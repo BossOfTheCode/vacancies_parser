5a4acfb467de0bbaaaff2d548b43ea0fd208ef14
BossOfTheCode <c.a.tulenov@gmail.com>
2022-05-12 21:25:14 +0500

    project created

---
+++ src/main/java/ru/welcometotheclub/vacanciesparser/VacanciesparserApplication.java
@@ -0,0 +1,13 @@
+package ru.welcometotheclub.vacanciesparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacanciesparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacanciesparserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/welcometotheclub/vacanciesparser/VacanciesparserApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.welcometotheclub.vacanciesparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacanciesparserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
