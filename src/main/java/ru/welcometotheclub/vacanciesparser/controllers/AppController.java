d730b06d2ba999bcc2df68eff90d5ba2ab3faca7
BossOfTheCode <c.a.tulenov@gmail.com>
2022-05-12 23:04:21 +0500

    Добавил контроллер

---
+++ src/main/java/ru/welcometotheclub/vacanciesparser/controllers/AppController.java
@@ -0,0 +1,13 @@
+package ru.welcometotheclub.vacanciesparser.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+}
